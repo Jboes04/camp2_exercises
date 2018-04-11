1337fc09b2d44d7d47540a9924f22212e3ff85b3
Boes Julian <julian.boes@decathlon.com>
2018-04-11 16:30:41 +0200

    ju

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -7,40 +7,11 @@
    - if the number is divisible by 3 and 5 : `FizzBuzz`
    - otherwise, the value is preserved.
 */
-// boucle for dans la fonction
+
 function fizzBuzz(list) {
-  list = [1, 2, 3, 4, 5, 6];
-  const message = "";
-  if( list % 3 === 0) {
-    console.log(message);
-  }
-  if ( list % 5 === 0) {
-    message += 'Buzz';
-  }
-  if( list % 3 === 0 && list % 5 === 0) {
-    message += 'FizzBuzz';
-  }
-  console.log( list );
+  
+
 }
 
-fizzBuzz();
-
-function fizzBuzz (list) {
-  const fbArray = [];
-  for ( let i = 0; i < list.length -1; i++) {
-    if (list[i]%3 === O && list[i]%5 === 0) {
-      fbArray.push ("FizzBuzz");
-    } else if ( list[i]%3 === 0){
-      fbArray.push ("Fizz");
-    } else if (list[i]%5 === 0) {
-      fbArray.push("Buzz");
-    } else {
-      fbArray.push(list[i]);
-    }
-  }
-  return fbArray;
-}
-
-console.log(FizzBuzz ([1, 15]));
 
 module.exports = fizzBuzz;
--- 02_programming_fundamentals/07_file_management/01_basic_fs/02_delete_file.js
+++ 02_programming_fundamentals/07_file_management/01_basic_fs/02_delete_file.js
@@ -2,5 +2,25 @@
 // if it's a file (do not remove a folder).
 //
 // The function returns a boolean indicating if it successfully removed the file.
+const path = require('path');
+const fs = require('fs');
 
+const pathfile = "/Users/Buzz/Workspace/camp2_exercises/02_programming_fundamentals/07_file_management/01_basic_fs/juliantest.js";
+
+function deleteFile (path) {
+  fs.stat(path, (error, stats) => {
+    if (error) {
+      console.warn(error);
+    } else ( stats.isFile(path));
+  });
+  fs.unlink(path, error => {
+    if(error) {
+      console.warn(error);
+      return;
+    }
+    console.log('File Deleted');
+  });
+}
+
+deleteFile(pathfile);
 module.exports = deleteFile
