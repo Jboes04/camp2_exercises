510ff7c815297550b589be415b450e7cb3be6101
Boes Julian <julian.boes@decathlon.com>
2018-04-17 18:15:54 +0200

    ju

--- 03_backend_developement/02_node_&_internet_fundamentals/02_manage_a_webservice/talk_with_json_placeholder.js
+++ 03_backend_developement/02_node_&_internet_fundamentals/02_manage_a_webservice/talk_with_json_placeholder.js
@@ -151,8 +151,36 @@ function updatePostTitle(postId, newTitle, callback){
   );
 }
 
+function updatePostBody(postId, newBody, callback){
+  request(
+    {
+      url: `http://jsonplaceholder.typicode.com/posts/${postId}`,
+      method: "PATCH",
+      form: {
+        "body": newBody,
+      }
+    },
+    function (error, response, result) {
+      callback(result);
+    }
+  );
+}
 
-
+function updatePost(postId, newTitle, newBody, callback) {
+  request(
+    {
+      url: `http://jsonplaceholder.typicode.com/posts/${postId}`,
+      method: "PATCH",
+      form: {
+        "title": newTitle,
+        "body": newBody
+      }
+    },
+    function (error, response, result) {
+      callback(result);
+    }
+  );
+}
 
 
 module.exports = {
@@ -166,5 +194,139 @@ module.exports = {
   fetchCommentsByPost: fetchCommentsByPost,
   publishPost: publishPost,
   publishComment: publishComment,
-  updatePostTitle: updatePostTitle
+  updatePostTitle: updatePostTitle,
+  updatePostBody: updatePostBody,
+  updatePost: updatePost
 };
+
+// Correction exercice :
+
+// const request = require("request");
+//
+// function fetchPosts(callback) {
+//   request.get(
+//     "http://jsonplaceholder.typicode.com/posts",
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchPostByUser(userId, callback) {
+//   request.get(
+//     `http://jsonplaceholder.typicode.com/users/${userId}/posts`,
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchPost(id, callback) {
+//   request.get(
+//     `http://jsonplaceholder.typicode.com/posts/${id}`,
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchUsers(callback) {
+//   request.get(
+//     "http://jsonplaceholder.typicode.com/users/",
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchUser(userId, callback) {
+//   request.get(
+//     `http://jsonplaceholder.typicode.com/users/${userId}`,
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchComments(callback) {
+//   request.get(
+//     "http://jsonplaceholder.typicode.com/comments",
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function fetchCommentsByPost(postId, callback) {
+//   request.get(
+//     `http://jsonplaceholder.typicode.com/posts/${postId}/comments`,
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function publishComment(postId, name, email, body, callback) {
+//   request.post(
+//     "http://jsonplaceholder.typicode.com/comments",
+//     {form: {postId: postId, name: name, email: email, body: body}},
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function publishPost(userId, title, body, callback) {
+//   request.post(
+//     "http://jsonplaceholder.typicode.com/posts",
+//     {form: {userId: userId, title: title, body: body}},
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function updatePostTitle(postId, newTitle, callback) {
+//   request.patch(
+//     `http://jsonplaceholder.typicode.com/posts/${postId}`,
+//     {form: {title: newTitle}},
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function updatePostBody(postId, newBody, callback) {
+//   request.patch(
+//     `http://jsonplaceholder.typicode.com/posts/${postId}`,
+//     {form: {body: newBody}},
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// function updatePost(postId, newTitle, newBody, callback) {
+//   request.patch(
+//     `http://jsonplaceholder.typicode.com/posts/${postId}`,
+//     {form: {title: newTitle, body: newBody}},
+//     function(error, response, body) {
+//       callback(body);
+//     }
+//   );
+// }
+//
+// module.exports = {
+//   updatePost: updatePost,
+//   updatePostBody: updatePostBody,
+//   updatePostTitle: updatePostTitle,
+//   publishComment: publishComment,
+//   publishPost: publishPost,
+//   fetchCommentsByPost: fetchCommentsByPost,
+//   fetchComments: fetchComments,
+//   fetchUsers: fetchUsers,
+//   fetchUser: fetchUser,
+//   fetchPost: fetchPost,
+//   fetchPostByUser: fetchPostByUser,
+//   fetchPosts: fetchPosts
+// };
---
+++ 03_backend_developement/02_node_&_internet_fundamentals/03_work_with_several_files/container.js
@@ -0,0 +1,18 @@
+
+const liters = {
+  litersOfCoffee: 0,
+}
+
+function putLitersOfCoffee(numberOfLiters) {
+
+}
+
+function consumeLitersOfCoffee(numberOfLiters){
+
+}
+
+
+
+module.exports = {
+
+};
